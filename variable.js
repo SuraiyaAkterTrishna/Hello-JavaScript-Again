af876cac838a088b4ba3272aff987ee5865dc7ca
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-20 23:23:15 +0600

    Five things to write a variable

--- variable.js
+++ variable.js
@@ -1,5 +1,14 @@
 /* IQ: What is variable?
-A variable is a value that can change, depending on conditions or on information passed to the program. */
+A variable is a value that can change, depending on conditions or on information passed to the program. 
+
+** Five things to write a variable: 
+1. var (The var statement declares a variable)
+2. Variable name (variables must be identified with unique names
+3. Assignment operator “=” (use an “=” operator to assign a value)
+4. Value (specifies the value to assign to the variable
+5. Semicolon ; (use a semicolon to indicate the end of var statement)
+
+*/
 
 
 var name = "Rony";
