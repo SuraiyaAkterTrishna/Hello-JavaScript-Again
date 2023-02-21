d223fef6509830c678e242e3b02f44029922175b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-21 16:30:16 +0600

    subtraction

--- math.js
+++ math.js
@@ -3,6 +3,11 @@ var orangePrice = 15;
 var applePrice = 20;
 
 var totalPrice = orangePrice + applePrice;
-console.log(totalPrice);
+// console.log(totalPrice);
 
 // subtraction
+var fishPrice = 1000;
+var previousFishPrice = 350;
+
+var revenueOfFish = fishPrice - previousFishPrice;
+// console.log(revenueOfFish);
