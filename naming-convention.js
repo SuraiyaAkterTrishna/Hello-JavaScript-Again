45ac4cbbbb903d82c8e76f1e6cb63d974f7a3f35
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-21 16:02:32 +0600

    variable name can not starts with a number but can ends with a number

--- naming-convention.js
+++ naming-convention.js
@@ -7,3 +7,7 @@ var return = true;
 
 //3. variable  name can not have quotation 
 var "name" = "Tom hanks";
+
+//4. variable name can not starts with a number but can ends with a number
+var 99club = 1962;
+var club99 = 1964;
