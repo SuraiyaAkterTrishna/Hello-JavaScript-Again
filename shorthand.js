c421fc157abfde3bdae91bae1a59d8c0f042d35c
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-21 17:01:00 +0600

    set a new value

--- shorthand.js
+++ shorthand.js
@@ -1,12 +1,16 @@
 // variable declaration
 var price = 99;
-console.log(price);
+// console.log(price);
 
 // variable value update
 price = 199;
-console.log(price);
+// console.log(price);
 
 // add something to the previous value
 var price1 = 60;
 var price2 = 80;
+
+// set a new value
+price1 = 90;
+console.log(price1);
 
