118d735b6bf56415d6c2e1180edb2d686af5fa67
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-21 17:35:43 +0600

    conversion- parseInt, parseFloat

---
+++ conversion.js
@@ -0,0 +1,15 @@
+var cat = "200";
+var dog = 200;
+var totalPetPrice = cat + dog;
+console.log(totalPetPrice);
+
+var fish = 350;
+var aquarium = '500';
+var totalSetupCost = fish + parseInt(aquarium);
+console.log(totalSetupCost);
+
+var gpa = "3.5";
+var gpaNumber = parseInt(gpa);
+console.log(gpaNumber);
+var gpaFloat = parseFloat(gpa);
+console.log(gpaFloat);
