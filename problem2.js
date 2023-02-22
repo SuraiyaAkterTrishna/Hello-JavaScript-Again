64858f5a0339bc83957a31122f22284ca1753534
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-22 19:42:59 +0600

    problem 2 done

---
+++ problem2.js
@@ -0,0 +1,10 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMark = ( math + biology + chemistry + physics + bangla );
+
+var averageMark = totalMark / 5;
+console.log(averageMark.toFixed(2));
